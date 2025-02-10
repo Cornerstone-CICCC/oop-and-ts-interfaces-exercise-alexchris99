0158c1949bde23f3072eba614371e58e3f1b28bb
Christian Jimenez <cajt@outlook.com>
2025-02-10 12:41:00 -0800

    Interfaces and Types

--- dist/ex05.js
+++ dist/ex05.js
@@ -1,18 +1,11 @@
+"use strict";
 // Exercise 5: Type Casting
 // Define an interface for a "User" with a required "id" (number) and optional "email" (string).
 // Create an object with unknown type, then cast it to the "User" interface and log the email if it exists.
-
-interface User {
-  id: number
-  email?: string
-}
-
-const unknownUser:User = {
-  id: 1,
-  email: "test@example.com"
+const unknownUser = {
+    id: 1,
+    email: "test@example.com"
 };
-
 const user = unknownUser;
-
 console.log(user.email || "Email not available");
 // Expected Output: "test@example.com"
